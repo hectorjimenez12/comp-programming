01a7425d48eaad5b7839340ab6906eec73204a18
hectorjimenez12 <72049668+hectorjimenez12@users.noreply.github.com>
2025-04-27 19:16:58 -0400

    Update bits.cpp

--- problems/bits.cpp
+++ problems/bits.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+int main() {
+    int n;
+    cin >> n; 
+
+    int val = 0;  // Initialize the value to 0
+    
+    string command;
+    for (int i = 0; i < n; ++i) {
+        cin >> command;  // Read the command (e.g., X++ or --X)
+      
+        if (command == "X++" || command == "++X") {
+            val++;  
+        } else if (command == "--X" || command == "X--") {
+            val--;  
+        }
+    }
+
+    cout << val << endl;  
+    return 0;
+}
